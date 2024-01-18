ec1816eb5f9f6ac8d1c4ee1bcb018b527cedece2
Miguel <miguel@Miguels-MacBook-Air.local>
2024-01-18 17:32:10 -0600

    Creacion de componentes de input task y archivo de la pagina principal

---
+++ pixelpayChallengeMR/src/MainPage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import InputTask from './components/InputTask.vue';
+</script>
+
+<template>
+    <InputTask></InputTask>
+</template>
+
+<style>
+</style>
---
+++ pixelpayChallengeMR/src/components/InputTask.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <form>
+        <input type="text">
+        <button> Agregar </button>
+    </form>
+</template>
+
+<style scoped>
+
+form{
+    max-width: 500px;
+    margin: 20px auto;
+    background: white;
+    text-align: left;
+    padding: 50px;
+    border-radius: 10px;
+}
+
+input {
+    display: block;
+    padding: 10px 6px;
+    width:100%;
+    border: none;
+    color: #555;
+    border-bottom: 1px solid #ddd;
+}
+</style>
